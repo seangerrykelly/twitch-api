94dd71625d352dc27fa31671ac20d0262a75edeb
Sean Kelly <seangerrykelly@gmail.com>
2021-05-10 17:56:54 -0400

    added users request

--- twitch-backend/routes/users.js
+++ twitch-backend/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+const API = require('../lib/API');
+const axios = require('axios');
 
-/* GET users listing. */
+var users = [];
+
+axios.get("https://api.twitch.tv/helix/users?id=59299632", {
+        headers: API.headers()
+    })
+    .then((response) => {
+        users = response.data;
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send(users);
 });
 
 module.exports = router;
